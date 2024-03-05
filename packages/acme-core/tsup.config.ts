07c8f6979c7c675128532da2a7f03acb24a2dba0
KateZ <hello.katezhang@gmail.com>
2024-03-05 02:06:23 -0500

    feat(template): Update to ignore test files in tsup conf

--- packages/acme-core/tsup.config.ts
+++ packages/acme-core/tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: {
     "index": "src/index.tsx",
-    "button": "src/Button.tsx",
-    "input": "src/Input.tsx",
+    "button": "src/button.tsx",
+    "input": "src/input.tsx",
   },
   banner: {
     js: "'use client'",

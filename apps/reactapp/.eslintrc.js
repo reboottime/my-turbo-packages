fa34f7f61f590b60888f008e0b7c56e3899a289f
KateZ <hello.katezhang@gmail.com>
2024-03-08 10:58:59 -0500

    Support create react app in mono repo

---
+++ apps/reactapp/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  extends: ["@acme/eslint-config/react-internal.js"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+  },
+  ignorePatterns: ["*.spec.tsx", "*.test.tsx", "*.spec.ts", "*.test.ts"],
+};
